23becd0bfb63d463822f38ea4f981f1c6864e32f
Andrei Borisov <clinri@ya.ru>
2021-11-08 22:11:43 +0300

    add Entity table with tasks

---
+++ app/src/main/java/ru/clinri/mertech_to_do/entities/ListWithToDo.kt
@@ -0,0 +1,26 @@
+package ru.clinri.mertech_to_do.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "task_list_item")
+data class TaskListItem(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo(name = "name_task") //задача
+    val nameTask: String,
+
+    @ColumnInfo(name = "info_task") //описание задачи
+    val infoTask: String,
+
+    @ColumnInfo(name = "deadline_date")
+    val deadlineDate: String,
+
+    @ColumnInfo(name = "complete")
+    val complete: Boolean = false,
+
+    ) : Serializable
