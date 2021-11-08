313b9872eac69e138d98c26d4e16904804549329
Andrei Borisov <clinri@ya.ru>
2021-11-08 23:57:09 +0300

    add menu bar with bottom

--- app/src/main/java/ru/clinri/mertech_to_do/activities/MainActivity.kt
+++ app/src/main/java/ru/clinri/mertech_to_do/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package ru.clinri.mertech_to_do.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import ru.clinri.mertech_to_do.R
+import ru.clinri.mertech_to_do.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.addTask -> {
+                    Log.d("MyLog", "Add task")
+                }
+            }
+            true
+        }
     }
 }
